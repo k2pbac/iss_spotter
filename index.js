e14484930896962532846e60633acf8da27d0592
k2pbac <krisbachan123@hotmail.com>
2021-08-26 15:48:18 -0400

    refactor

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
   console.log(passTimes);
 });
 
